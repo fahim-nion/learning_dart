3fe98dc992d8ad1a194ee0a1f1d50d10fdd889ad
fahim-nion <fahimalhassan4@gmail.com>
2025-10-08 01:04:02 +0600

    dart data types

--- dart_prac_1/bin/dart_data_types.dart
+++ dart_prac_1/bin/dart_data_types.dart
@@ -19,5 +19,24 @@ void main(){
   var intNumber = 10;
   var doubleNumber = 20.3;
   var sum = intNumber + doubleNumber;
-  print(sum);
+  // print(sum);
+
+  //Dart String
+  //single quote and double quote strings are available
+  var singleStr = 'hi my name is nion';
+  var doubleStr = "i am from Mymensingh";
+  // print(singleStr +" "+ doubleStr);
+
+
+  //boolean
+  var next = true;
+  var prev = false;
+
+  if (next == true){
+    // print("yeah positive");
+  }
+
+
+  //list data types
+  
 }
