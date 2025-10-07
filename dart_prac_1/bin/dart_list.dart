be9eb29ff1acbebba9882cd5f3ea8f3504eb6021
fahim-nion <fahimalhassan4@gmail.com>
2025-10-08 03:19:02 +0600

    list property

---
+++ dart_prac_1/bin/dart_list.dart
@@ -0,0 +1,12 @@
+//list property
+
+void main(){
+  var city = ["dhaka","kurigram","bogra","bandorban"];
+  print(city.length);
+  print(city.reversed);
+  print(city.first);
+  print(city.last);
+  print(city.hashCode);
+  print(city.isEmpty);
+  print(city.isNotEmpty);
+}
