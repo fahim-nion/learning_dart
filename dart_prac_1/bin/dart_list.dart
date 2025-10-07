5f6bb33a504e6cb818fd8be644b01b3d2e6a9dbf
fahim-nion <fahimalhassan4@gmail.com>
2025-10-08 03:31:39 +0600

    list property

--- dart_prac_1/bin/dart_list.dart
+++ dart_prac_1/bin/dart_list.dart
@@ -1,12 +1,35 @@
 //list property
 
+//fixed length list [const kore dilei hobe fixed hoye jabe ,no new elements can be added there]
+//growable length list [keeping var before the variable name makes them vulnerable to adding elements which means we have a growable list]
+
+
+//list insertion in a growable list
+
 void main(){
   var city = ["dhaka","kurigram","bogra","bandorban"];
-  print(city.length);
-  print(city.reversed);
-  print(city.first);
-  print(city.last);
-  print(city.hashCode);
-  print(city.isEmpty);
-  print(city.isNotEmpty);
+  // print(city.length);
+  // print(city.reversed);
+  // print(city.first);
+  // print(city.last);
+  // print(city.hashCode);
+  // print(city.isEmpty);
+  // print(city.isNotEmpty);
+  city.add("cox-bazar");
+  print(city);
+
+
+  //add(), addAll(), insert(), insertAll()
+  city.add("mymensingh");
+  print(city);
+
+  city.addAll(["comilla","foridpur","kushtia"]);
+  print(city);
+
+  // city.insert(index, element)
+  city.insert(1, "Manikganj");
+  print(city);
+
+  city.insertAll(0, ["Muktagachha","sylhet"]);
+  print(city);
 }
