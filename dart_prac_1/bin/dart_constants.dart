d4747beeb8e94f94de8d90fc376207a9e0a4c1c3
fahim-nion <fahimalhassan4@gmail.com>
2025-10-08 03:10:59 +0600

    dart constants

---
+++ dart_prac_1/bin/dart_constants.dart
@@ -0,0 +1,10 @@
+//constants are meant to something that does not changes
+/* definable criteria is:
+  can define with them with 
+      "final a = 10";
+      "const a = 20";
+
+      immutable object
+      
+*/
+
