246283f9796017a6a55a879cd6ceb17e3b04d789
fahim-nion <fahimalhassan4@gmail.com>
2025-10-08 00:53:29 +0600

    dart data types

---
+++ dart_prac_1/bin/dart_data_types.dart
@@ -0,0 +1,23 @@
+// dart data types 
+/* 
+- Number 
+- strings
+- boolean
+- Lists
+- Maps
+- Runes
+- Symbols
+*/
+
+/*Dart Number:
+- integer
+- double [decimal, float]
+*/
+
+
+void main(){
+  var intNumber = 10;
+  var doubleNumber = 20.3;
+  var sum = intNumber + doubleNumber;
+  print(sum);
+}
