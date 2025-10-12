24c0160325084db52d7f29fe7b1f1bb1fe0a11b1
fahim-nion <fahimalhassan4@gmail.com>
2025-10-12 09:50:31 +0600

    map properties

--- dart_prac_1/bin/dart_map.dart
+++ dart_prac_1/bin/dart_map.dart
@@ -8,4 +8,41 @@ void main(){
   };
   
   print(person);
+
+  //how can i add a new key value pair?
+
+  person['country'] = 'Bangladesh';
+
+  //for accessing only the one associated with specific k:v bond
+
+  print(person['country']);
+
+  //----------------------Advanced Topic------------------------------------
+
+
+  /* now if i want to create the map without using the curly braces
+    we can do it using map() constructor. 
+ */
+
+  var student = new Map();
+  student['name'] = 'Fahim';
+  student['role'] = 'Admin';
+  student['code'] = 'BGA729c';
+  student['course'] = 'DART';
+
+  print(student);
+
+  //all the use case methods are same
+
+  //suppose we want to remove a specific one
+  student['use'] = 'nothing';
+  print(student);
+  
+
+  student.remove('use');
+  print(student);
+
+  student.addAll({'salary':'24444 USD','nomination': 'rising'});
+  print(student);
+
 }
