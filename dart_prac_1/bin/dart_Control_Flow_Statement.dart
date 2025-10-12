e47e68e32c75f34eb10cf55b4847a37ecb57e8e1
fahim-nion <fahimalhassan4@gmail.com>
2025-10-12 10:01:29 +0600

    controll flow of dart program

--- dart_prac_1/bin/dart_Control_Flow_Statement.dart
+++ dart_prac_1/bin/dart_Control_Flow_Statement.dart
@@ -1,9 +1,12 @@
 /*
 the controll statements or flow of controll statements are used to controll 
 the flow of Dart program. In Dart, control flow statement can be categorized in 3 following ways
-  - decision making statements
-  - looping statements
-  - jump statements
+  - decision making statements [if,if-else,if-else-if, switch]
+  - looping statements 
+  - jump statements [break, conntinue]
 
 */
 
+void main(){
+  //decision making
+}
