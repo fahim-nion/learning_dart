9286598983bcc3039fcd0b2a4fab77ea0479e1d3
fahim-nion <fahimalhassan4@gmail.com>
2025-10-13 21:03:52 +0600

    dart OOP

--- dart_prac_1/bin/dart OOP.dart
+++ dart_prac_1/bin/dart OOP.dart
@@ -1,6 +1,20 @@
+// class
+// Object
+// Inheritence
+// Polymorphism
+// Interfaces
+// Abstract class
 
 
+
+class MyClass{
+  //accessing variables from class
+  var myName = "taju";
+}
 
 void main(){
   print("Hii Object Oriented Programming!");
+  //creating an object
+  var myObj = new MyClass();
+  print(myObj.myName);
 }
