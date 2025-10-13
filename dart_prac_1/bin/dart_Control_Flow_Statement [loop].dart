5f2d62c4b849d12703175783b24ff62e2b10473e
fahim-nion <fahimalhassan4@gmail.com>
2025-10-13 13:44:24 +0600

    dart control flow statement

--- dart_prac_1/bin/dart_Control_Flow_Statement [loop].dart
+++ dart_prac_1/bin/dart_Control_Flow_Statement [loop].dart
@@ -9,10 +9,36 @@ do-while
 
 void main() {
   for (int i = 1; i <= 10; i++) {
-    print("Saying yeahh for $i time");
+    // print("Saying yeahh for $i time");
   }
 
 
-  //for in loop
-  
+  //for in loop 
+
+  var list1 = [10,20,30,45,84,89,90];
+  //it always applies on a list
+  //for in loop er limit dite hoy na
+  for (var elem in list1) {
+    // print(elem);
+  }
+
+  /* how we can apply for in loop a map{json array}*/
+  var people = [
+    {'name':'abdul','home': 'dhaka','age' : 27},
+    {'name':'jobbar','home': 'mymensingh','age' : 25},
+    {'name':'shorif','home': 'comilla','age' : 22}
+    ];
+
+    //when we keep a number of maps [object/dictionary] in a list/array we call it a json
+    
+
+  for (var elem in people) {
+    // print("${elem['name']} ${elem['age']}");
+  }
+
+  var a_set = {'bob','maehem','bishop'};
+  for (var e in a_set) {
+    print(e);
+  }
 }
+
