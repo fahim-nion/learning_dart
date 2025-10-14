6335640753d26bcfc2337ec4de91d43837ff1a39
fahim-nion <fahimalhassan4@gmail.com>
2025-10-14 23:02:18 +0600

    dart asynchronous programming

--- dart_prac_1/bin/dart Asynchronous programming.dart
+++ dart_prac_1/bin/dart Asynchronous programming.dart
@@ -1,10 +1,22 @@
 // synchronous programming e each line gets executed line by line
-//asynchronous
+//asynchronous(Future (2 state : Completed | Incompleted), Stream-class, async, await)
 
 void main(){
   print("Line 1");
   print("Line 2");
-  
+  Future fetchData() async {
+    Future.delayed(Duration(seconds: 3), ()=> Oiii());
+  }
+  fetchData();
   print("Line 4");
   print("Line 5");
+}
+
+Oiii(){
+  print("i am in Oii & calling moreWait()");
+  moreWait();
+}
+
+Future moreWait() async {
+  Future.delayed(Duration(seconds: 4),()=>print("Line 3"));
 }
