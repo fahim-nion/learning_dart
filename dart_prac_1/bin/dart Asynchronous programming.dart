a3b108ee5b1d4a9ec076a36409376fcf99a6c9e1
fahim-nion <fahimalhassan4@gmail.com>
2025-10-14 22:49:48 +0600

    dart asynchronous programming

---
+++ dart_prac_1/bin/dart Asynchronous programming.dart
@@ -0,0 +1,10 @@
+// synchronous programming e each line gets executed line by line
+//asynchronous
+
+void main(){
+  print("Line 1");
+  print("Line 2");
+  
+  print("Line 4");
+  print("Line 5");
+}
