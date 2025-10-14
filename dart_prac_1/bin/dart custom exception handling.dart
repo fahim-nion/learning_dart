78dd86c3db04b198b8f8e0a34a9ed9414e1b5cb9
fahim-nion <fahimalhassan4@gmail.com>
2025-10-14 20:37:00 +0600

    dart exceptions handling

--- dart_prac_1/bin/dart custom exception handling.dart
+++ dart_prac_1/bin/dart custom exception handling.dart
@@ -1,4 +1,25 @@
 void main(){
+  //custom exeception handling
+  value(7);
+}
 
-  
+
+class Value implements Exception{
+  String lessThanFive(){
+    return "value can not be less than 5";
+  }
+  String greaterThanFive(){
+    return "value must have to be within 5-10";
+  }
+}
+
+
+value(int v){
+  if (v<5){
+    throw Value().lessThanFive();
+  } else if (v > 10){
+    throw Value().greaterThanFive();
+  }else{
+    print("Successfull");  
+  }
 }
