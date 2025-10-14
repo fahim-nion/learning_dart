4269be4b863277e8365968aea35d6cb58e7cfe40
fahim-nion <fahimalhassan4@gmail.com>
2025-10-14 08:37:55 +0600

    dart OOP

--- dart_prac_1/bin/dart-debugging.dart
+++ dart_prac_1/bin/dart-debugging.dart
@@ -8,7 +8,16 @@
 */
 
 
-var main(){
-  
-
+void main(){
+  var x = 10;
+  var y = 20;
+  var add = x+y;
+  var subs = y-x;
+  var mul = y*x;
+  var div = y/x;
+  var mod = y%x;
+  print(add);
+  print(subs);
+  print(mul);
+  print(div);
 }
