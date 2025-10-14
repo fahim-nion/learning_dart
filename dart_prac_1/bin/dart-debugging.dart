9b169139f053d945dc1d8e8e29bc525625221345
fahim-nion <fahimalhassan4@gmail.com>
2025-10-14 08:12:40 +0600

    dart debugging

---
+++ dart_prac_1/bin/dart-debugging.dart
@@ -0,0 +1,14 @@
+/*
+- to find out my mistake
+- to test my code
+- to understand complex program flow
+- to work with complex action part by part
+- to improve my code
+
+*/
+
+
+var main(){
+  
+
+}
