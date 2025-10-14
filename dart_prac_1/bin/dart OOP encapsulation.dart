8c5bb67ff06382afb69476360c49bc8653674892
fahim-nion <fahimalhassan4@gmail.com>
2025-10-14 23:29:12 +0600

    dart OOP encapsulation

---
+++ dart_prac_1/bin/dart OOP encapsulation.dart
@@ -0,0 +1,25 @@
+void main(){
+  var obj = Father();
+  obj.setAge = 50;
+  print(obj.getAge);
+}
+
+
+class Father{
+  int? _age;
+  
+  
+  set setAge(int a){
+    _age = a;
+  }
+
+
+  get getAge{
+    return _age;
+  }
+}
+
+
+//private variable hoye geche "_property" shamne "_" dilei hoye jay private
+// to access them we need to use setter and getter method;
+
