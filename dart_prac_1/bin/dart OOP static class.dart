ac0e4a7ef1b9e8b5235cbc3e1067d5ce03731818
fahim-nion <fahimalhassan4@gmail.com>
2025-10-14 23:07:34 +0600

    dart OOP

--- dart_prac_1/bin/dart OOP static class.dart
+++ dart_prac_1/bin/dart OOP static class.dart
@@ -1,4 +1,11 @@
 void main(){
-  
-  
+  // object create kora chara ee jodi class er nam dhore deke name paite chai
+  print(Example.name);
+  var obj = Example();
+  print(obj.name2);
+}
+
+class Example{
+  static String name = "Fahim Morshed";
+  String name2 = "Taju";
 }
