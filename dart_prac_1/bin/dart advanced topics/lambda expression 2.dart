2cb5c5ee0d5ece0104ebf6ccfbd968b9f55743b2
fahim-nion <fahimalhassan4@gmail.com>
2025-10-22 00:57:03 +0600

    implenting lambda expression and high order function

--- dart_prac_1/bin/dart advanced topics/lambda expression 2.dart
+++ dart_prac_1/bin/dart advanced topics/lambda expression 2.dart
@@ -7,12 +7,12 @@
 // or can do both
 
 void main(){
-  Function ammuFunction = (a,b)=>print(a+b);
-  babaFunction("Tomar ammu koi?", ammuFunction);
+  Function momFunc=(a,b)=>print(a+b);
+  babaFunction("Tomar ammu koi?", momFunc); // ei function ta te ami string ar function pathalam ekta
 }
 
 
-void babaFunction(String msg, Function ammuFunction){
-  print("Abbu Bollo:${msg}");
-  ammuFunction(20,30);
+void babaFunction(String msg, Function parameterFunc){   // msg ar function ta rcv krlam
+  print("Abbu Bollo: ${msg}");                           // msg display korte dikam
+  parameterFunc(20,30);                                  // received func e call dilam
 }
