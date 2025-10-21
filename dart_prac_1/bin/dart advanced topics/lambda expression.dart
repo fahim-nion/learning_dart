49d94b64b4728e529a5cccdda720be263120047e
fahim-nion <fahimalhassan4@gmail.com>
2025-10-21 23:57:31 +0600

    advanced topics lambda expression

---
+++ dart_prac_1/bin/dart advanced topics/lambda expression.dart
@@ -0,0 +1,15 @@
+// dart lambda expression function 
+// a function in dart is an object
+
+/*it is a function without a name also known as anonymous function  */
+
+
+void main(){
+  print("Hello Darkness My Old Friend!!");
+  addMyNum(12, 9);
+}
+
+addMyNum(int a, int b){
+  var sum = a+b;
+  print(sum);
+}
