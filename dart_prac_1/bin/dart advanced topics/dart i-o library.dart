35190846cc68ec2093c43553f7a3dc667bc908a5
fahim-nion <fahimalhassan4@gmail.com>
2025-10-25 02:03:08 +0600

    dart I/O library

---
+++ dart_prac_1/bin/dart advanced topics/dart i-o library.dart
@@ -0,0 +1,23 @@
+/*
+
+🔹 dart:io library means
+
+It’s a built-in library in Dart that allows your program to do input and output (I/O) operations —
+that is, to read or write data from the console, files, or network.
+
+🔸 In even simpler terms:
+
+dart:io lets your Dart code talk to the outside world.
+
+🔹 Examples of what it can do:
+Task	How dart:io helps
+Take user input from keyboard	stdin.readLineSync()
+Print output to console	stdout.write() / print()
+Read or write files	File('data.txt').readAsString()
+Make or receive network requests	HttpClient, HttpServer
+Run system commands	Process.run()
+Get system info	Platform.operatingSystem
+
+
+
+*/
