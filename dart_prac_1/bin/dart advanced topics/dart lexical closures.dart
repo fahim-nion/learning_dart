bb20111aa24c28fbac6225b1b3298232f6aa80f7
fahim-nion <fahimalhassan4@gmail.com>
2025-10-25 00:38:57 +0600

    dart advanced topics - lexical closures

---
+++ dart_prac_1/bin/dart advanced topics/dart lexical closures.dart
@@ -0,0 +1,8 @@
+// closure is a special function
+// in java we are not allowed to modify the parent scope variables but in dart we can that feature is called closure
+
+
+void main(){
+  String msg = "dart is good";
+  
+}
