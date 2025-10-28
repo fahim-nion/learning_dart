ecc82f0dc276e1e826dd26c3dd27d887c96ee3e0
fahim-nion <fahimalhassan4@gmail.com>
2025-10-29 03:09:21 +0600

    dart advanced topics on creating directory

---
+++ dart_prac_1/bin/dart advanced topics/dart directory.dart
@@ -0,0 +1,15 @@
+/*     
+
+
+
+*/
+
+
+import 'dart:io';
+
+void main() {
+  Directory dir = Directory("D:\\BOOKS and courses\\Flutter Course\\dart_prac_1\\bin\\dart advanced topics\\new_folder_for_dart_prac");
+  
+  dir.createSync();
+  print("✅ Directory created successfully!");
+}
