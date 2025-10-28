603dabf307e9a80499ce9aeda53537d84f369d02
fahim-nion <fahimalhassan4@gmail.com>
2025-10-29 04:12:44 +0600

    dart advanced topics on mixin

--- dart_prac_1/bin/dart advanced topics/dart mixin.dart
+++ dart_prac_1/bin/dart advanced topics/dart mixin.dart
@@ -3,3 +3,13 @@
 // mixin er kono object toiri korte parbo na
 // mixin e kono constructor use korte parbo na
 // can not be extended
+mixin Mixin1{
+  
+}
+mixin Mixin2{
+
+}
+
+class Animal with Mixin1, Mixin2{
+  
+}
