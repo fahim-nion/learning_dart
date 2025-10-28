b055e5aa00642f97e21bd5abe5cc67241cc76ab2
fahim-nion <fahimalhassan4@gmail.com>
2025-10-29 02:59:24 +0600

    dart advanced topics on delete and rename file

--- dart_prac_1/bin/dart advanced topics/dart file delete-rename.dart
+++ dart_prac_1/bin/dart advanced topics/dart file delete-rename.dart
@@ -3,8 +3,11 @@ import "dart:io";
 
 void main() async{
   File f = new File("D:\\BOOKS and courses\\Flutter Course\\dart_prac_1\\bin\\dart advanced topics\\unwanted-file.txt");
+  
   File new_f = new File("D:\\BOOKS and courses\\Flutter Course\\dart_prac_1\\bin\\dart advanced topics\\unwanted-file-rename.txt");
+  
   await f.writeAsString("So this is a test file");
+  
   await new_f.writeAsString("So this is a test file to learn file renaming");
   print("The File has been created! Now I'll call a function to delete it as well");
   await deleteFile(f);
