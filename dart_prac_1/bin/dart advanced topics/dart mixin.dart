b36c0bc959927276a1ab1b4b34520a47424b1519
fahim-nion <fahimalhassan4@gmail.com>
2025-10-29 04:08:24 +0600

    updated advanced topics.txt

---
+++ dart_prac_1/bin/dart advanced topics/dart mixin.dart
@@ -0,0 +1,5 @@
+// mixin use kora hoy so that we can use the same code again and again
+
+// mixin er kono object toiri korte parbo na
+// mixin e kono constructor use korte parbo na
+// can not be extended
