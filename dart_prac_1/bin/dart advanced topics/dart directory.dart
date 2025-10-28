65676e9b2ae77d82f03e27058e134246e9193801
fahim-nion <fahimalhassan4@gmail.com>
2025-10-29 03:12:53 +0600

    dart advanced topics on creating directory

--- dart_prac_1/bin/dart advanced topics/dart directory.dart
+++ dart_prac_1/bin/dart advanced topics/dart directory.dart
@@ -1,6 +1,14 @@
 /*     
 
+nested directory create korar jonno obviously "recursive: true" dbo
+eta shob file o delete kore dey
 
+Method	         Type	        Description
+create()	      Async	     Creates folder, returns Future<Directory>
+createSync()   	Sync	     Creates folder instantly
+exists()	      Async	     Checks if folder exists
+delete()	      Async	       Deletes folder
+recursive:true  Flag       Creates/deletes nested folders
 
 */
 
@@ -9,7 +17,7 @@ import 'dart:io';
 
 void main() {
   Directory dir = Directory("D:\\BOOKS and courses\\Flutter Course\\dart_prac_1\\bin\\dart advanced topics\\new_folder_for_dart_prac");
-  
+
   dir.createSync();
   print("✅ Directory created successfully!");
 }
