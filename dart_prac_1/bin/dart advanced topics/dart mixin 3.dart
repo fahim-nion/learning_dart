ceaf70c5c723d3dc9afcf51e18f0ce98b1be1a7f
fahim-nion <fahimalhassan4@gmail.com>
2025-10-29 20:25:57 +0600

    dart advanced topics on mixin

--- dart_prac_1/bin/dart advanced topics/dart mixin 3.dart
+++ dart_prac_1/bin/dart advanced topics/dart mixin 3.dart
@@ -1,6 +1,6 @@
 abstract class Animal{
-    String name;
-    double speed;
+    final String name;
+    final double speed;
 
     //constructor
     Animal(this.name, this.speed);
@@ -8,7 +8,20 @@ abstract class Animal{
     void run();
 }
 
-// this can run function can be used for a specific class which is Animal class
+// this canRun function can be used for a specific class which is Animal class
 mixin canRun on Animal{
+    @override
+    void run(){
+        print("$name is running at speed $speed");
+    }
+}
 
+
+class Dog extends Animal with canRun{
+    Dog(super.name,super.speed);
+}
+
+void main(){
+    final doggy = Dog("burky", 10);
+    doggy.run();
 }
