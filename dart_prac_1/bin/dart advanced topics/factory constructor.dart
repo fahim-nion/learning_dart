e5cd876770230493e5c8c81c2bd4b12450b83bdc
fahim-nion <fahimalhassan4@gmail.com>
2025-10-31 22:58:36 +0600

    dart advanced topics on factory constructor

--- dart_prac_1/bin/dart advanced topics/factory constructor.dart
+++ dart_prac_1/bin/dart advanced topics/factory constructor.dart
@@ -6,3 +6,28 @@
 //similar to static method 
 //must return instance
 
+/*
+
+Normally, constructors (ClassName()) always create a new instance of a class.
+But sometimes you might not want a new object every time — you might want to:
+
+- Return an existing object (like a cache or singleton)
+
+- Return a subclass
+
+- Do some logic before creating the object
+
+That’s what a factory constructor allows.
+
+*/
+
+
+class ClassName {
+  factory ClassName() {
+    // You can return an instance of this class
+    // or a subclass or even an existing object
+    return ClassName._internal(); // example
+  }
+
+  ClassName._internal(); // private named constructor
+}
