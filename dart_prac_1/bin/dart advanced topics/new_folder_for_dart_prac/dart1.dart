9eed86d5e61389d87f78a3808df7d09f67a8e5d0
fahim-nion <fahimalhassan4@gmail.com>
2026-02-28 06:25:09 +0600

    getting on with the revision

---
+++ dart_prac_1/bin/dart advanced topics/new_folder_for_dart_prac/dart1.dart
@@ -0,0 +1,17 @@
+void main(){
+  print("Hello World!");
+  //variable
+  var hi = 5;
+  var doubleDatatype = 5.5343;
+  print(doubleDatatype);
+  print(hi);
+  var aString = "this is a string";
+  print(aString);
+  var intiger = 21;
+  print(intiger);
+  var boolean = true;
+  print(boolean);
+  //list
+  var myList = ["mango", "apple", "banana"];
+  print(myList[2]);
+}
