67bcff43d12d40493ab840559782400e39ef324c
fahim-nion <fahimalhassan4@gmail.com>
2026-03-03 06:56:37 +0600

    revision

---
+++ dart_prac_1/bin/dart advanced topics/new_folder_for_dart_prac/dart5.dart
@@ -0,0 +1,14 @@
+void main(){
+  //for loop
+
+  // for(int i = 0; i<=5; i++){
+  //   print("Hello World!");
+  // }
+
+  //while loop
+  int i=0;
+  while (i<5) {
+    print("Hwllo while loop!");
+    i+=1;
+  }
+}
